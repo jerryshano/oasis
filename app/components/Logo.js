57d557a18018ef036d2f389dd2c125936e72c0cc
jerrykankelborg <jkankelborg@csumb.edu>
2025-02-15 17:55:31 -0800

    Get the app started with Nav

---
+++ app/components/Logo.js
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-4 z-10">
+      <img src="/logo-light.png" height="60" width="60" alt=""/>
+      <span className="text-xl font-semibold text-primary-100">the wild oassis</span>
+    </a>
+  )
+}
+export default Logo;
